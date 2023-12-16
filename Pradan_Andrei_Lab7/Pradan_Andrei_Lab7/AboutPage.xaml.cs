d5cfa2242143ca61f0781940d4e15a7558355038
Pradan Andrei <d695960@cyso-028504.burda.com>
2023-12-16 16:26:16 +0200

    Laborator 7

---
+++ Pradan_Andrei_Lab7/Pradan_Andrei_Lab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Pradan_Andrei_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Pradan_Andrei_Lab7/Pradan_Andrei_Lab7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pradan_Andrei_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
